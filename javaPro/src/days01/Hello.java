eb5714c3555bdd6fe65e725b2cd197928cea5b58
dbxodnsms <57580401+dbxodnsms@users.noreply.github.com>
2021-03-03 12:07:22 +0900

    Hi Coding Add

--- javaPro/src/days01/Hello.java
+++ javaPro/src/days01/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		// 강의실에서 추가 코딩
+		System.out.println("Hi!");
 	}
 
 }
